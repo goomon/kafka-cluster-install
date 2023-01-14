4a941116b71ff2dc5171af20404322e2bb8332c4
goomon <jaegoo.jho@gmail.com>
2023-01-14 21:22:57 +0900

    Feat: broker install script

--- broker1/install.sh
+++ broker1/install.sh
@@ -9,8 +9,8 @@ curl -O https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz
 tar xvf kafka_2.12-2.8.0.tgz
 rm -rf kafka_2.12-2.8.0.tgz
 
-cd /kafka_2.12-2.8.0
-mv /server.properties /kafka_2.12-2.8.0/config/server.properties
+cd kafka_2.12-2.8.0
+mv /kafka-cluster-install/broker1/server.properties kafka_2.12-2.8.0/config/server.properties
 
 # Run kafka
 export KAFKA_JMX_OPTS='-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false 
@@ -18,4 +18,4 @@ export KAFKA_JMX_OPTS='-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxre
   -Dcom.sun.management.jmxremote.port=9999 
   -Dcom.sun.management.jmxremote.rmi.port=9999 
   -Djava.rmi.server.hostname=0.0.0.0'
-env JMX_PORT=9999 /kafka_2.12-2.8.0/bin/kafka-server-start.sh -daemon /kafka_2.12-2.8.0/config/server.properties
+env JMX_PORT=9999 kafka_2.12-2.8.0/bin/kafka-server-start.sh -daemon kafka_2.12-2.8.0/config/server.properties
